59538edf63167567c7047ea7658569fabe457e57
Dhirender Choudhary <Dhirenderchoudhary0001@gmail.com>
2026-06-16 17:05:33 +0530

    chore: remove test files

--- test-corsair-db.ts
+++
@@ -1,4 +0,0 @@
-import { corsair } from "./src/server/corsair";
-const tenant = corsair.withTenant("dev");
-console.log("DB keys:", Object.keys(tenant.gmail.db));
-console.log("Messages keys:", Object.keys(tenant.gmail.db.messages));
--- test-corsair-gmail.ts
+++
@@ -1,3 +0,0 @@
-import { corsair } from "./src/server/corsair";
-const tenant = corsair.withTenant("dev");
-console.log("gmail keys:", Object.keys(tenant.gmail));
